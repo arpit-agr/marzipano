f7d0cd4a9b4d6b6d4cce0fe4876a5e19d1feb354
arpit-agr <arpit.agrawal2502@gmail.com>
2022-08-02 19:08:39 +0530

    add assets, content and styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,16 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
 const htmlmin = require("html-minifier");
 const CleanCSS = require("clean-css");
+const esbuild = require("esbuild");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setQuietMode(true);
   eleventyConfig.addPlugin(directoryOutputPlugin);
 
   //Passthrough copy
+	eleventyConfig.addPassthroughCopy("./src/scripts");
+	eleventyConfig.addPassthroughCopy("./src/images");
   // eleventyConfig.addPassthroughCopy("./src/fonts");
-	// eleventyConfig.addPassthroughCopy("./src/images");
-	// eleventyConfig.addPassthroughCopy("./src/scripts");
   // eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 	// eleventyConfig.addPassthroughCopy("./src/icon.svg");
 	// eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png");
@@ -18,8 +19,8 @@ module.exports = function(eleventyConfig) {
 	// eleventyConfig.addPassthroughCopy("./src/manifest.webmanifest");
 
   //Watch target
-	// eleventyConfig.addWatchTarget("./src/_includes/css/");
-	// eleventyConfig.addWatchTarget('./src/scripts/');
+	eleventyConfig.addWatchTarget("./src/_includes/css/");
+	eleventyConfig.addWatchTarget('./src/_includes/scripts/');
 
   //Filter
   eleventyConfig.addFilter("cssmin", function(code) {
@@ -29,6 +30,14 @@ module.exports = function(eleventyConfig) {
     else {
       return code
     }
+  });
+  eleventyConfig.addFilter("addNbsp", (str) => {
+    if (!str) {
+      return;
+    }
+    let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
+    title = title.replace(/"(.*)"/g, '\\"$1\\"');
+    return title;
   });
 
   //Transforms
@@ -45,6 +54,19 @@ module.exports = function(eleventyConfig) {
     }
 
     return content;
+  });
+
+  //ELEVENTY AFTER EVENT
+	eleventyConfig.on('eleventy.after', async () => {
+    // Run me after the build ends
+		return esbuild.build({
+      entryPoints: [
+				'src/_includes/scripts/index.js'
+			],
+      bundle: true,
+			minify: true,
+      outdir: 'public/scripts'
+    });
   });
 
   return {
--- src/index.md
+++ src/index.md
@@ -1,6 +1,11 @@
 ---
-title: Hello World
+title: "Sunset on Cerro Paranal"
 layout: "layouts/home.html"
+image:
+  link: "ESO_Paranal_360_Marcio_Cabral_Chile_08-CC.jpeg"
+  credit:
+    name: "ESO"
+    link: "https://www.eso.org/public/images/ESO_Paranal_360_Marcio_Cabral_Chile_08-CC/"
 ---
 
-# {{ title }}
+# {{ title | addNbsp | safe }}
